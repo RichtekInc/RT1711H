2f7ba22dd74a26311b53d72ea52ba218595953d6
lilian_wang(王友伶) <lilian_wang@richtek.com>
2017-10-20 14:53:30 +0800

    v019 init

---
+++ kernel/include/linux/power/rt-charger.h
@@ -0,0 +1,18 @@
+/*
+ *  include/linux/power/rt-charger.h
+ *
+ *  Copyright (C) 2015 Richtek Technology Corp.
+ *  Jeff Chang <jeff_chang@richtek.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ */
+
+#ifndef __LINUX_RT_CHARGER_H
+#define __LINUX_RT_CHARGER_H
+
+extern int tcpc_check_vsafe0v(struct tcpc_device *tcpc);
+
+#endif /* __LINUX_RT_CHARGER_H */
